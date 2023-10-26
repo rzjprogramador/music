da00b3c2188321ed19a9756196e9a1bfa28b21b5
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-10-26 15:45:46 -0300

    feat: estrategias composicao

---
+++ MUSIC__by_dart/compor/estrategias.compor.dart
@@ -0,0 +1,14 @@
+/*
+- harmonia :
+  definir_progressao:
+    parte_A :
+      comeco_grave:
+      exemplos_graus: [ "1564 I V vi IV em notas D A bm G ",]
+      menores_dentro: "1", termina_em: "em maior"
+
+    parte_B :
+    conceito: "faça variações da parte_A , "
+    variar_o_que_fez_na_parte_A : [ "fazendo ao contrario da parte_A : vi IV V ex: bm G A", ]
+
+- nuances na melodia da voz - sobe e desce na vóz dentro da nota.
+*/

b613511b99b1ef5bc04b25a6cf83abb681b2ab6a
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-10-26 17:08:22 -0300

    composicao CONFIA

--- MUSIC__by_dart/compor/estrategias.compor.dart
+++ MUSIC__by_dart/compor/estrategias.compor.dart
@@ -5,10 +5,36 @@
       comeco_grave:
       exemplos_graus: [ "1564 I V vi IV em notas D A bm G ",]
       menores_dentro: "1", termina_em: "em maior"
+      tempo_que_vai_explorar_nota_por_nota : "4",
+      tipo_de_rima: [ "1 p2 3 r2" ]
 
     parte_B :
     conceito: "faça variações da parte_A , "
     variar_o_que_fez_na_parte_A : [ "fazendo ao contrario da parte_A : vi IV V ex: bm G A", ]
 
 - nuances na melodia da voz - sobe e desce na vóz dentro da nota.
+
+
+---
+CONFIA
+
+CONTA PRA MIM TEUS SEGREDOS
+QUE EU TENHO A TARDE TODA...
+NÃO FIQUE NA MAOS DOS OUTROS
+DEIXA DE SER BOBA...
+
+QUEM TE ENGANOU ... NÃO FOI EU
+QUEM TE MAGOOU ... SÓ PERDEU
+
+SEI QUE JA SOFREU DECEPÇÃO DEMAIS
+MAS COMIGO O GAME É TRANSPARENTE
+QUANDO A PESSOA É ESPECIAL
+O TRATAMENTO É DIFERENTE
+
+NAO VOU TE DECEPCIONAR ... CONFIA ..CONFIA
+PODEM ME TORTURAR ... CONFIA CONFIA
+
+QUE TÁ GUARDADO O TEU SEGREDO.
+
+
 */
